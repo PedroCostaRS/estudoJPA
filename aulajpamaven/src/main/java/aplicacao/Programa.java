edf35f2b1a635191f8b51670e655bec1c769ee65
PedroCostaRS <tecpedrocosta@gmail.com>
2022-10-25 22:25:10 -0300

    Aula JPA MAVEN introdução

---
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,21 @@
+package aplicacao;
+
+import dominio.Pessoa;
+
+public class Programa {
+	
+	public static void main (String[] args) {
+		
+		Pessoa p1 = new Pessoa(1, "Pedro", "tecpedrocosta@gmail.com");
+		Pessoa p2 = new Pessoa(2, "Rosilda", "Rosaparmesao@gmail.com");
+		Pessoa p3 = new Pessoa(3, "Joel", "PrJoelRodrigues@gmail.com");
+	
+	
+	System.out.println(p1);
+	System.out.println(p2);
+	System.out.println(p3);
+	
+	
+	}
+
+}
